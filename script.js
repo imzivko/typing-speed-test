93a04e4c975706dc4a1bf4bda283ce5bc8b6e219
imzivko <68650120+imzivko@users.noreply.github.com>
2021-03-13 21:35:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const startButton = document.getElementById("start-button");
 const statsDiv = document.getElementById("stats");
 const textDiv = document.getElementById("text");
 const clickSound = new Audio("sounds/buttonClick.mp3");
-const inputText = document.getElementById("input-space");
+const timeStarted = document.getElementById("time-start");
 
 // Split words.js text into letters and put them in a newly created span element in HTML
 const textIntoCharacters = textLib.split("");
@@ -26,6 +26,10 @@ let cursorPosition = 0;
 let cursorCharacter = characters[cursorPosition];
 cursorCharacter.classList.add("cursor");
 
+// Tracking right/wrong input just for display after game ends
+let wrongLetters = 0;
+let correctLetters = 0;
+
 // Initial time set to null
 let startTime = null;
 let endTime = null;
@@ -33,17 +37,17 @@ let endTime = null;
 // Starting the game
 startButton.addEventListener("click", function () {
   clickSound.play();
-  inputText.focus();
   // Displaying the typing speed and typing text
   statsDiv.classList.add("hide");
   textDiv.classList.remove("hide");
   startButton.classList.add("hide");
 
-  // Listening for button press, starting the time and calculating wPM
-  const keyListener = document.addEventListener("keydown", ({ key }) => {
+  // Listening for button press, starting the time
+  const keyListener = document.addEventListener("keypress", ({ key }) => {
     console.log(key);
+    timeStarted.innerHTML = "TIMER ACTIVATED";
 
-    // Timer starts when the user starts typing
+    // Time starts when the user starts typing
     if (!startTime) {
       startTime = new Date();
     }
@@ -53,11 +57,16 @@ startButton.addEventListener("click", function () {
       cursorCharacter.classList.remove("wrong");
       cursorCharacter.classList.add("done");
       cursorCharacter = characters[++cursorPosition];
+      correctLetters++;
     } else {
       cursorCharacter.classList.add("wrong");
+      cursorCharacter.classList.remove("done");
+      cursorCharacter.classList.remove("cursor");
+      cursorCharacter = characters[--cursorPosition];
+      wrongLetters++;
     }
 
-    // Calculations (time, wps, wpm..)
+    // Calculations, ending time
     if (cursorPosition >= characters.length) {
       endTime = new Date();
       const timeElapsed = endTime - startTime;
@@ -65,11 +74,15 @@ startButton.addEventListener("click", function () {
       const wordNumber = textLib.split(" ").length;
       const wps = (wordNumber / seconds).toFixed(1);
       const wpm = (wps * 60).toFixed(1);
+
+      timeStarted.classList.add("hide");
       textDiv.classList.add("hide");
       statsDiv.classList.remove("hide");
       statsDiv.textContent = `${wpm}, ${wps}`;
 
-      // Disable input listener at the end of the test
+      correctLetters -= 2 * wrongLetters;
+
+      // Disable input listener on game end
       document.removeEventListener("keydown", keyListener);
       return;
     }
