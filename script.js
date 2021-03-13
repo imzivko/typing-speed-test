cc0629bba6f335b740615c5a6b5d31b5569c1561
imzivko <68650120+imzivko@users.noreply.github.com>
2021-03-13 18:54:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 const startButton = document.getElementById("start-button");
 const statsDiv = document.getElementById("stats");
 const textDiv = document.getElementById("text");
+const clickSound = new Audio("sounds/buttonClick.mp3");
+const inputText = document.getElementById("input-space");
 
 // Split words.js text into letters and put them in a newly created span element in HTML
 const textIntoCharacters = textLib.split("");
@@ -30,12 +32,14 @@ let endTime = null;
 
 // Starting the game
 startButton.addEventListener("click", function () {
+  clickSound.play();
+  inputText.focus();
   // Displaying the typing speed and typing text
-  statsDiv.classList.remove("hide");
+  statsDiv.classList.add("hide");
   textDiv.classList.remove("hide");
   startButton.classList.add("hide");
 
-  // Listening for button press, starting the time and calculating wPM speed
+  // Listening for button press, starting the time and calculating wPM
   const keyListener = document.addEventListener("keydown", ({ key }) => {
     console.log(key);
 
@@ -51,6 +55,23 @@ startButton.addEventListener("click", function () {
       cursorCharacter = characters[++cursorPosition];
     } else {
       cursorCharacter.classList.add("wrong");
+    }
+
+    // Calculations (time, wps, wpm..)
+    if (cursorPosition >= characters.length) {
+      endTime = new Date();
+      const timeElapsed = endTime - startTime;
+      const seconds = (timeElapsed / 1000).toFixed(1);
+      const wordNumber = textLib.split(" ").length;
+      const wps = (wordNumber / seconds).toFixed(1);
+      const wpm = (wps * 60).toFixed(1);
+      textDiv.classList.add("hide");
+      statsDiv.classList.remove("hide");
+      statsDiv.textContent = `${wpm}, ${wps}`;
+
+      // Disable input listener at the end of the test
+      document.removeEventListener("keydown", keyListener);
+      return;
     }
 
     cursorCharacter.classList.add("cursor");
